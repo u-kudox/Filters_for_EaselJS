c2540ebb7e45882478b4b3d8c16bbd7d2f911799
u-kudox <kudox.jp@gmail.com>
2014-01-21 14:06:21 +0900

    Setting grunt

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -5,22 +5,41 @@ module.exports = function(grunt) {
 			glowfilter : {
 				src : ['../src/GlowFilter.js'],
 				dest : '../lib/glowfilter.min.js'
+			},
+			dropshadowfilter : {
+				src : ['../src/DropShadowFilter.js'],
+				dest : '../lib/dropshadowfilter.min.js'
 			}
 		},
 		copy : {
 			glowfilter : {
 				files : [{expand:true, cwd:'../lib/', src:'glowfilter.min.js', dest:'../examples/js/'}]
+			},
+			dropshadowfilter : {
+				files : [{expand:true, cwd:'../lib/', src:'dropshadowfilter.min.js', dest:'../examples/js/'}]
 			}
 		},
 		watch : {
 			glowfilter : {
 				files : ['../src/GlowFilter.js'],
 				tasks : ['glowfilter']
+			},
+			dropshadowfilter : {
+				files : ['../src/DropShadowFilter.js'],
+				tasks : ['dropshadowfilter']
+			}
+		},
+		clean : {
+			docs : {
+				src : ['yuidoc', '../docs'],
+				options : {
+					force : true
+				}
 			}
 		},
 		yuidoc : {
 			docs : {
-				name : 'Filters for EaselJS',
+				name : 'Filters for EaselJS API documentation',
 				description : '<%= pkg.description %>',
 				version : '<%= pkg.version %>',
 				url : '<%= pkg.url %>',
@@ -43,9 +62,11 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-yuidoc');
 	grunt.loadNpmTasks('grunt-contrib-compress');
 
-	grunt.registerTask('docs', ['yuidoc:docs', 'compress:docs']);
+	grunt.registerTask('docs', ['clean:docs', 'yuidoc:docs', 'compress:docs']);
 	grunt.registerTask('glowfilter', ['uglify:glowfilter', 'copy:glowfilter']);
+	grunt.registerTask('dropshadowfilter', ['uglify:dropshadowfilter', 'copy:dropshadowfilter']);
 };
